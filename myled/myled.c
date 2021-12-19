f77b9fdff739ba84b5860c1a364e913883b9e10e
korasyo <95561892+korasyo@users.noreply.github.com>
2021-12-19 15:12:54 +0900

    Update myled.c

--- myled/myled.c
+++ myled/myled.c
@@ -5,7 +5,7 @@
 #include <linux/uaccess.h>
 #include <linux/io.h>
 
-MODULE_AUTHOR("RyuiCHI Ueda & Keita yamada");
+MODULE_AUTHOR("Ryuichi Ueda & Keita yamada");
 MODULE_DESCRIPTION("driver for LED control");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.0.1");
